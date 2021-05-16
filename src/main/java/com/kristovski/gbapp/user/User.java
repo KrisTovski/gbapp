1b89bad2547683fdd3b58a7a7a55fd2fcdefb2d1
KrisTovski <kfilak@onet.eu>
2021-05-16 14:10:29 +0200

    ref chagnelogs and sql

--- src/main/java/com/kristovski/gbapp/user/User.java
+++ src/main/java/com/kristovski/gbapp/user/User.java
@@ -5,7 +5,6 @@ import lombok.*;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
