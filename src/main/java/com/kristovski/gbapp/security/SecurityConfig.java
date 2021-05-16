d2701a9f344c75dcac9a8cd8e6a0a74a10c1a6ed
KrisTovski <kfilak@onet.eu>
2021-05-16 17:45:17 +0200

    add security and the DelegatingPasswordEncoder bean

--- src/main/java/com/kristovski/gbapp/security/SecurityConfig.java
+++ src/main/java/com/kristovski/gbapp/security/SecurityConfig.java
@@ -1,18 +1,31 @@
 package com.kristovski.gbapp.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .authorizeRequests()
                 .antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/h2-console/..").permitAll();
+                .authorizeRequests().antMatchers("/h2-console/..").permitAll()
+                .antMatchers("/register").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
         http.csrf().disable();
         http.headers().frameOptions().disable();
     }
