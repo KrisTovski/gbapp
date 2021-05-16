32ae79f2c7912d60e11dfb53e6c28496fa116e1c
KrisTovski <kfilak@onet.eu>
2021-05-16 13:41:38 +0200

    add insert users file sql

---
+++ src/main/resources/db/version/2021-05-16/02-insert-users.sql
@@ -0,0 +1,6 @@
+insert into user (id, login, first_name, last_name, email, password, enable, create_time, update_time, locked) values
+(1, 'mylogin', 'Brandon', 'Howorth', 'bhoworth0@devhub.com', 'mypassword', 0, '2020-01-26T13:14:05', null, 0),
+(2, 'tlally1', 'Torrin', 'Lally', 'tlally1@163.com', 'x79tnUhGk', 1, '2020-01-26T13:14:05', null, 0),
+(3, 'fdorling2', 'Frederigo', 'Dorling', 'fdorling2@theglobeandmail.com', 'M8gg8PD', 0, '2020-01-26T13:14:05', null, 0),
+(4, 'gfalk3', 'Gradey', 'Falk', 'gfalk3@dailymotion.com', 'iQHgtEMczLGr', 0, '2020-01-26T13:14:05', null, 1),
+(5, 'mettridge4', 'Melisa', 'Ettridge', 'mettridge4@europa.eu', '5TAS4AFb', 0, '2020-01-26T13:14:05', null, 0);
