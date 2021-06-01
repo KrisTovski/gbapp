a0c1e1aba9086ac564965d5508cb1bdd12a46af4
KrisTovski <kfilak@onet.eu>
2021-06-01 20:34:00 +0200

    ref

---
+++ src/main/java/com/kristovski/gbapp/booking/CalendarController.java
@@ -0,0 +1,13 @@
+package com.kristovski.gbapp.booking;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CalendarController {
+
+    @GetMapping("/calendar")
+    public String calendar() {
+        return "calendar";
+    }
+}
