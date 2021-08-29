978bbebb57682a4285d2dbc077f16c27de4b3254
KrisTovski <kfilak@onet.eu>
2021-08-29 17:11:44 +0200

    add booking to controller

--- src/main/java/com/kristovski/gbapp/booking/BookingController.java
+++ src/main/java/com/kristovski/gbapp/booking/BookingController.java
@@ -3,8 +3,10 @@ package com.kristovski.gbapp.booking;
 import com.kristovski.gbapp.user.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookingController {
@@ -18,10 +20,26 @@ public class BookingController {
         this.userService = userService;
     }
 
+    @GetMapping("/booking")
+    public String calendar(Model model) {
+        model.addAttribute("booking", new Booking());
+        return "booking";
+    }
+
+    @PostMapping
+    public String saveBooking(Booking booking, Model model){
+        model.addAttribute("booking", booking);
+        return "/panel/bookings";
+    }
+
+
     @GetMapping("/panel/deleteBooking/{id}")
     public String deleteBooking(@PathVariable(value = "id") Long id) {
         bookingService.deleteBookingById(id);
         return "redirect:/panel/bookings";
     }
+
+
+
 
 }
